185625cc52de6b708be3e4fea5072c216b56a06b
NewDeanPlus <Mareepikachu@gmail.com>
2023-03-16 12:28:10 -0400

    Added autonomous again

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
@@ -93,6 +94,10 @@ public class DriveTrain extends SubsystemBase {
 
     public DifferentialDrive getDifferentialDrive(){
         return d;
+    }
+
+    public CommandBase autoDrive(){
+        d.tankDrive(-.8, -.8);
     }
 
     //other stuff is mostly encoders, gyro, acc
